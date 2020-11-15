445a1981ddc8e9beb32f5c429a4469898b8ed0bd
KaiBartolone <kbartolo@stanford.edu>
2020-11-15 18:21:42 -0500

    modified:   src/tests/test_nfc.c

--- src/tests/test_nfc.c
+++ src/tests/test_nfc.c
@@ -5,8 +5,22 @@
  */
 
 #include <uart.h>
+#include <spi.h>
+#include <gpio.h>
+#include <pn532.h>
+
+const unsigned int RESET_PIN = GPIO_PIN20;
+const unsigned int NSS_PIN = GPIO_PIN21;
+
+void test_spi_transfer()
+{
+    char *frame[20];
+    memcpy(frame, 0, 20);
+}
 
 void main(void)
 {
+    pn532_init();
+    test_spi_transfer();
     uart_putchar(EOT);
 }
