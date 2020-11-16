61699730e64faf2aca951fc6ed572a57701a774f
KaiBartolone <kbartolo@stanford.edu>
2020-11-15 19:43:10 -0500

    changed console_printf to printf

--- src/tests/test_nfc.c
+++ src/tests/test_nfc.c
@@ -5,10 +5,11 @@
  */
 
 #include <uart.h>
-<<<<<<< HEAD
 #include <spi.h>
 #include <gpio.h>
 #include <pn532.h>
+#include <timer.h>
+#include <printf.h>
 
 const unsigned int RESET_PIN = GPIO_PIN20;
 const unsigned int NSS_PIN = GPIO_PIN21;
@@ -17,38 +18,19 @@ void test_spi_transfer()
 {
     char *frame[20];
     memcpy(frame, 0, 20);
-=======
-#include <pn532.h>
-#include <assert.h>
-#include <console.h>
-#include <fb.h>
-#include <gl.h>
-#include <timer.h>
-#include "printf.h"
-#include "pn532.h"
+}
 
 void basic_tests(void)
 {
-    console_init(40, 40);
-    console_printf("\n----------------------------------------\n");
-    console_printf("|           NFC Basic Testing          |\n");
-    console_printf("|   CONSOLE SIZE: 40 x 40 (in chars)   |\n");
-    console_printf("----------------------------------------\n");
->>>>>>> 8e9316de716064c7fc9bb3cc337a372dad4ed882
+    printf("\n----------------------------------------\n");
+    printf("|           NFC Basic Testing          |\n");
+    printf("|   CONSOLE SIZE: 40 x 40 (in chars)   |\n");
+    printf("----------------------------------------\n");
 }
 
 void main(void)
 {
-<<<<<<< HEAD
-    pn532_init();
+    pn532_init(RESET_PIN, NSS_PIN);
     test_spi_transfer();
-=======
-    uart_init();
-    timer_init();
-
-    printf("Testing NFC\n");
-    basic_tests();
-
->>>>>>> 8e9316de716064c7fc9bb3cc337a372dad4ed882
     uart_putchar(EOT);
 }
