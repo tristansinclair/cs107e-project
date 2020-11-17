c6bbe45c2fec1b8752d1630995a811e0f4fa8adc
KaiBartolone <kbartolo@stanford.edu>
2020-11-17 13:48:19 -0500

    fixed command spelling error added print_bytes

--- include/nfc.h
+++ include/nfc.h
@@ -15,4 +15,11 @@
  */
 void nfc_init(unsigned int reset_pin, unsigned int nss_pin);
 
+/**
+ * @fn print_bytes
+ * ---------------------
+ * Prints buffer of bytes in a nice readable format.
+ */
+void print_bytes(byte_t *buf, size_t bufsize);
+
 #endif // _NFC_H
--- src/lib/nfc.c
+++ src/lib/nfc.c
@@ -10,3 +10,14 @@ void nfc_init(unsigned int reset_pin, unsigned int nss_pin)
 {
     pn532_init(reset_pin, nss_pin);
 }
+
+void print_bytes(byte_t *buf, size_t bufsize)
+{
+    for (int i = 0; i < bufsize; i++)
+    {
+        if (i % 16 == 0)
+            printf("\n%d : ", i % 16);
+        printf("%2x ", buf[i]);
+    }
+    printf("\n");
+}
