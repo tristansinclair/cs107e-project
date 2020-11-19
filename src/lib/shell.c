1dac7d8ae10ead1f06a7cb0c9fb09bdc5806956a
KaiBartolone <kbartolo@stanford.edu>
2020-11-19 17:22:12 -0500

    implemented cmd functions: read tag, set tag, check tag

--- src/lib/shell.c
+++ src/lib/shell.c
@@ -22,75 +22,43 @@ static const command_t commands[] = {
     {"poke", "[address] [value] store value into memory at address", cmd_poke},
     {"charge", "[value] charges tag with value", cmd_charge_tag},
     {"read", "[block number] prints block", cmd_read_tag},
-    // {"pay", "[value] pays tag with value", cmd_pay_tag},
-    // {"set", "[value] sets tag balance", cmd_set_tag_value},
-    // {"check", "checks tag balance", cmd_check_tag_balance},
+    {"pay", "[value] pays tag with value", cmd_pay_tag},
+    {"set", "[value] sets tag balance", cmd_set_tag_value},
+    {"check", "checks tag balance", cmd_check_tag_balance},
 };
 static const size_t COMMAND_SIZE = sizeof(commands) / sizeof(commands[0]);
 
-// static void print_bytes(uint8_t *buf, size_t bufsize)
-// {
-//     // Print vertical line numbers
-//     shell_printf("\n     ");
-//     int num_length = bufsize < 16 ? bufsize : 16;
-//     for (int i = 0; i < num_length; i++)
-//     {
-//         if (i > 9)
-//             shell_printf("%d ", i);
-//         else
-//             shell_printf(" %d ", i);
-//     }
-
-//     for (int i = 0; i < bufsize; i++)
-//     {
-//         if (i % 16 == 0)
-//             shell_printf("\n%02d : ", i / 16);
-//         shell_printf("%02x ", buf[i]);
-//     }
-//     shell_printf("\n");
-// }
-
-int cmd_charge_tag(int argc, const char *argv[])
+static void print_bytes(uint8_t *buf, size_t bufsize)
 {
-    if (argc == 2)
+    // Print vertical line numbers
+    shell_printf("\n     ");
+    int num_length = bufsize < 16 ? bufsize : 16;
+    for (int i = 0; i < num_length; i++)
     {
-        shell_printf("Please scan your card!\n");
-        int *balance = 0;
-
-        int error_code = get_balance(balance);
-        if (error_code != PN532_ERROR_NONE)
-        {
-            shell_printf("Error: 0x%02x\r\n", error_code);
-            return 1;
-        }
-
-        *balance -= strtonum(argv[1], NULL);
-
-        error_code = set_balance(*balance);
-        if (error_code != PN532_ERROR_NONE)
-        {
-            shell_printf("Error: 0x%02x\r\n", error_code);
-            return 1;
-        }
-
-        shell_printf("New Balance: %d\n", *balance);
-        return -1;
+        if (i > 9)
+            shell_printf("%d ", i);
+        else
+            shell_printf(" %d ", i);
     }
-    else
+
+    for (int i = 0; i < bufsize; i++)
     {
-        shell_printf("error: charge takes 1 argument [cost]\n");
-        return 1;
+        if (i % 16 == 0)
+            shell_printf("\n%02d : ", i / 16);
+        shell_printf("%02x ", buf[i]);
     }
+    shell_printf("\n");
 }
 
-int cmd_read_tag(int argc, const char *argv[])
+int cmd_check_tag_balance(int argc, const char *argv[])
 {
     // Check that command has no args
     if (argc != 1)
     {
-        shell_printf("error: read takes no arguments\n");
+        shell_printf("Error: read takes no arguments\n");
         return 1;
     }
+
     shell_printf("Please scan your card!\n");
     int *balance = 0;
 
@@ -102,7 +70,118 @@ int cmd_read_tag(int argc, const char *argv[])
     }
 
     shell_printf("Current Balance: %d\n", *balance);
-    return -1;
+    return 0;
+}
+
+int cmd_set_tag_value(int argc, const char *argv[])
+{
+    if (argc != 2)
+    {
+        shell_printf("Error: charge takes 1 argument [value]\n");
+        return 1;
+    }
+
+    shell_printf("Please scan your card!\n");
+    int *balance = 0;
+    int error_code = PN532_ERROR_NONE;
+
+    *balance += strtonum(argv[1], NULL);
+    error_code = set_balance(*balance);
+
+    if (error_code != PN532_ERROR_NONE)
+    {
+        shell_printf("Error: 0x%02x\r\n", error_code);
+        return 1;
+    }
+
+    shell_printf("New Balance: %d\n", *balance);
+    return 0;
+}
+
+int cmd_pay_tag(int argc, const char *argv[])
+{
+    if (argc != 2)
+    {
+        shell_printf("Error: charge takes 1 argument [value]\n");
+        return 1;
+    }
+
+    shell_printf("Please scan your card!\n");
+    int *balance = 0;
+
+    int error_code = get_balance(balance);
+    if (error_code != PN532_ERROR_NONE)
+    {
+        shell_printf("Error: 0x%02x\r\n", error_code);
+        return 1;
+    }
+
+    *balance += strtonum(argv[1], NULL);
+
+    error_code = set_balance(*balance);
+    if (error_code != PN532_ERROR_NONE)
+    {
+        shell_printf("Error: 0x%02x\r\n", error_code);
+        return 1;
+    }
+
+    shell_printf("New Balance: %d\n", *balance);
+    return 0;
+}
+
+int cmd_charge_tag(int argc, const char *argv[])
+{
+    if (argc != 2)
+    {
+        shell_printf("Error: charge takes 1 argument [value]\n");
+        return 1;
+    }
+
+    shell_printf("Please scan your card!\n");
+    int *balance = 0;
+
+    int error_code = get_balance(balance);
+    if (error_code != PN532_ERROR_NONE)
+    {
+        shell_printf("Error: 0x%02x\r\n", error_code);
+        return 1;
+    }
+
+    *balance -= strtonum(argv[1], NULL);
+
+    error_code = set_balance(*balance);
+    if (error_code != PN532_ERROR_NONE)
+    {
+        shell_printf("Error: 0x%02x\r\n", error_code);
+        return 1;
+    }
+
+    shell_printf("New Balance: %d\n", *balance);
+    return 0;
+}
+
+int cmd_read_tag(int argc, const char *argv[])
+{
+    uint8_t *response[1024];
+    size_t response_length = 0;
+
+    if (argc == 1)
+    {
+        response_length = 1024;
+        get_tag_info(response, response_length);
+    }
+    else if (argc == 2)
+    {
+        response_length = 16;
+        get_block_info(response, strtonum(argv[1], NULL));
+    }
+    else
+    {
+        shell_printf("Error: charge takes either 1 [block number] or no arguments\n");
+        return 1;
+    }
+
+    print_bytes(response, response_length);
 }
 
 /**
